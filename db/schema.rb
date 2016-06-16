39fbc4da9ff662e08d76755303edcc748bbe5cb4
Rails Udemy Course <sebastian.alvarado@mail.utoronto.ca>
2016-06-15 22:30:27 -0500

    Styles and JS are working partially

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@
 ActiveRecord::Schema.define(version: 20160615041002) do
 
   create_table "articles", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
